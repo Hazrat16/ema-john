ce0a00f8909fa124ffd152ac80545bda2bdf2852
Hazrat16 <hazrat17016@gmail.com>
2021-03-30 12:21:42 +0600

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxA0-C7Db08ZRwKPhprMB6LrHdg68TH9c",
+    authDomain: "ema-john-eco.firebaseapp.com",
+    projectId: "ema-john-eco",
+    storageBucket: "ema-john-eco.appspot.com",
+    messagingSenderId: "885036329002",
+    appId: "1:885036329002:web:6c012068e5e6ae89e27b53"
+  };
+export default firebaseConfig;
